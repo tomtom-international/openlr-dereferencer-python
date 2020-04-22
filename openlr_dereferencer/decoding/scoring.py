38d10884bcc88889d4a40b8f279cb1c1e6a36e7d
Jan Ole Zabel <janole.zabel@tomtom.com>
2020-04-22 10:25:01 +0200

    Fix bearing angle calculation

--- openlr_dereferencer/decoding/scoring.py
+++ openlr_dereferencer/decoding/scoring.py
@@ -64,17 +64,6 @@ def score_geolocation(
         return 1.0 - dist / radius
     return 0.0
 
-
-def get_bearing_point(candidate: Line, reverse: bool = False) -> Coordinates:
-    "Gets the point to which the bearing angle is computed."
-    if candidate.length < BEAR_DIST:
-        return candidate.end_node.coordinates
-    coordinates = list(candidate.coordinates())
-    if reverse:
-        coordinates.reverse()
-    return project_along_path(coordinates, BEAR_DIST)
-
-
 def score_angle_difference(angle1: float, angle2: float) -> float:
     """Helper for `score_bearing` which scores the angle difference.
 
@@ -91,13 +80,13 @@ def score_bearing(wanted: LocationReferencePoint, candidate: Line, is_last_lrp: 
     """Scores the difference between expected and actual bearing angle.
 
     A difference of 0° will result in a 1.0 score, while 180° will cause a score of 0.0."""
-    point1 = candidate.start_node.coordinates
-    point2 = get_bearing_point(candidate)
-    bear = degrees(bearing(point1, point2))
-    expected_bearing = BEAR_MULTIPLIER * wanted.bear
+    coordinates = list(candidate.coordinates())
     if is_last_lrp:
-        return score_angle_difference(expected_bearing, bear - 180)
-    return score_angle_difference(expected_bearing, bear)
+        coordinates.reverse()
+    point1 = coordinates[0]
+    point2 = project_along_path(coordinates, BEAR_DIST)
+    bear = degrees(bearing(point1, point2))
+    return score_angle_difference(wanted.bear, bear)
 
 
 def score_lrp_candidate(
