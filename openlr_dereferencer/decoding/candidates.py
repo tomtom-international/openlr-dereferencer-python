5f49df7c0f6a8d8225de144ac0aadd1b2fb2bbd3
Jan Ole Zabel <janole.zabel@tomtom.com>
2020-07-15 10:31:32 +0200

    Add a new observer event: failed matches

--- openlr_dereferencer/decoding/candidates.py
+++ openlr_dereferencer/decoding/candidates.py
@@ -175,4 +175,6 @@ def match_tail(
         except LRDecodeError as e:
             continue
 
+    if observer is not None:
+        observer.on_matching_fail(current, next_lrp, candidates, next_candidates)
     raise LRDecodeError("Decoding was unsuccessful: No candidates left or available.")
