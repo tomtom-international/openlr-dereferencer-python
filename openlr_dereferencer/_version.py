b6b25353ca04b3100183c5de3c856756d900fa5a
Stephen Curran <stephen.curran@tomtom.com>
2021-06-24 11:31:45 +0200

    Released v.1.2.0 version

--- openlr_dereferencer/_version.py
+++ openlr_dereferencer/_version.py
@@ -2,7 +2,7 @@
 __title__ = "openlr_dereferencer"
 __description__ = "OpenLR location dereferencer"
 __url__ = "https://github.com/tomtom-international/openlr-dereferencer-python"
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 __author__ = "Jan Ole Zabel"
 __author_email__ = "janole.zabel@tomtom.com"
 __license__ = "Apache 2.0"
