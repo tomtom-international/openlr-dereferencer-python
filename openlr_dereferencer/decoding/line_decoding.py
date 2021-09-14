79fb50e147953f52651cb4b0e43e82ff24140a9b
Bezaye Tesfaye Belayneh <belayneh@de3lxl-111982.ttg.global>
2021-09-14 13:49:56 +0200

    comment out prev.function calls

--- openlr_dereferencer/decoding/line_decoding.py
+++ openlr_dereferencer/decoding/line_decoding.py
@@ -5,6 +5,7 @@ from openlr import LineLocationReference, LocationReferencePoint
 from ..maps import MapReader
 from ..observer import DecoderObserver
 from .candidate_functions import nominate_candidates, match_tail
+from .bi_directional_candidate_search import bi_directional_search
 from .line_location import build_line_location, LineLocation
 from .routes import Route
 from .configuration import Config
@@ -17,20 +18,20 @@ def dereference_path(
         observer: Optional[DecoderObserver]
 ) -> List[Route]:
     "Decode the location reference path, without considering any offsets"
-    forwd_lrp, backwd_lrp = lrps[0], lrps[-1]
-    forwd_candidates, backwd_candidates = list(nominate_candidates(forwd_lrp, reader, config, False)), list(nominate_candidates(backwd_lrp, reader, config, False))
+    #forwd_lrp, backwd_lrp = lrps[0], lrps[-1]
+    #forwd_candidates, backwd_candidates = list(nominate_candidates(forwd_lrp, reader, config, False)), list(nominate_candidates(backwd_lrp, reader, config, False))
     
     
     # print("candidates: ", x=[i for i in first_candidates.score])
 
-    if observer is not None:
-        observer.on_candidates_found(first_lrp, first_candidates)
+    #if observer is not None:
+    #    observer.on_candidates_found(first_lrp, first_candidates)
 
     # linelocationpath = match_tail(first_lrp, first_candidates, lrps[1:], reader, config, observer)
-    linelocationpath = match_tail(forwd_lrp, backwd_lrp, forwd_candidates, backwd_candidates, lrps, reader, config, observer)
+    # linelocationpath = match_tail(forwd_lrp, backwd_lrp, forwd_candidates, backwd_candidates, lrps, reader, config, observer)
+    bi_directional_search(lrps, reader, config, observer)
 
-
-    return linelocationpath
+    #return linelocationpath
 
 
 def decode_line(reference: LineLocationReference, reader: MapReader, config: Config,
@@ -41,5 +42,5 @@ def decode_line(reference: LineLocationReference, reader: MapReader, config: Con
 
     # print("Number of lrps: ", len(reference.points), [i for i in reference.points])
     # print("\n lrp_lon lrp_lat lrp_frc lrp_fow lrp_bear lrp_lfrcnp lrp_dnp cand_link_id cand_dir cand_fow cand_frc cand_geo_score cand_fow_score cand_frc_score cand_bear cand_bear_diff cand_score min_score")
-    parts = dereference_path(reference.points, reader, config, observer)
-    return build_line_location(parts, reference)
+    dereference_path(reference.points, reader, config, observer)
+    #return build_line_location(parts, reference)
