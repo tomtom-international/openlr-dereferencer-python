b95502b8691c198c2eab13474d9582366d0bb3f2
Max Gardner <gardner.max@gmail.com>
2023-11-29 21:07:03 +0000

    changes to maps

--- openlr_dereferencer/maps/a_star/tools.py
+++ openlr_dereferencer/maps/a_star/tools.py
@@ -2,7 +2,8 @@
 
 from functools import lru_cache
 from ..abstract import Node
-from ..wgs84 import distance
+from .. import wgs84
+from .. import equal_area as ee
 
 
 class LRPathNotFoundError(Exception):
@@ -10,11 +11,15 @@ class LRPathNotFoundError(Exception):
 
 
 @lru_cache(maxsize=2)
-def heuristic(current: Node, target: Node) -> float:
+def heuristic(current: Node, target: Node, equal_area: bool = False) -> float:
     """Estimated cost from current to target.
 
     We use geographical distance here as heuristic here."""
-    return distance(current.coordinates, target.coordinates)
+    if not equal_area:
+        dist = wgs84.distance(current.coordinates, target.coordinates)
+    else:
+        dist = ee.distance(current.coordinates, target.coordinates)
+    return dist
 
 
 def tautology(_) -> bool:
